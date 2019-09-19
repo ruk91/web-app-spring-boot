c499b3990be7979b18851085aff4f96d30f2dcf1
Sajith Wijerathne <sajith@vizuamatix.com>
2019-09-19 17:34:42 +0530

    passed data using ModelAndView

--- src/main/java/com/ruk91/demo/HomeController.java
+++ src/main/java/com/ruk91/demo/HomeController.java
@@ -1,20 +1,22 @@
 package com.ruk91.demo;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
-	public String home(@RequestParam("name") String myName, HttpSession session) {
+	public ModelAndView home(@RequestParam("name") String myName) {
 				
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		
 		System.out.println("Hello! "+ myName);
-		session.setAttribute("name", myName);
-		return "home";
+		return mv;
 	}
 	
 
